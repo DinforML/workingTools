2251f3107b99bee21025072355e9db16cf39b078
DinforML <105668107+DinforML@users.noreply.github.com>
2022-05-27 03:48:58 +0800

    Update getFlow.py

--- getFlow.py
+++ getFlow.py
@@ -4,6 +4,7 @@ import os
 import time
 import csv
 import json
+import zipfile
 import requests
 import pandas as pd
 from tqdm import tqdm
@@ -12,9 +13,11 @@ from getpass import getpass
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+
 
 option = webdriver.ChromeOptions()
 option.add_argument('--disable-gpu')
@@ -22,8 +25,11 @@ option.add_argument("--disable-software-rasterizer")
 option.add_experimental_option('useAutomationExtension', False)
 option.add_experimental_option("excludeSwitches", ["enable-logging"])
 option.add_experimental_option('excludeSwitches', ['enable-automation'])
+option.add_experimental_option("prefs", {"credentials_enable_service": False,"profile.password_manager_enabled": False})
+capabilities = DesiredCapabilities.CHROME
+capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
 
-local_version = 'v1.1.0'
+local_version = 'v1.2.0'
 url = "https://api.github.com/repos/DinforML/workingTools/releases/latest"
 download_url = "https://github.com/DinforML/workingTools/releases/download/%s/default.zip"
 
@@ -35,25 +41,26 @@ def check_version():
 		print(f"当前版本为最新版本[ {local_version} ] ！")
 	else:
 		name = 'WorkingTools ' + online_version.replace('/','_') + '.zip'
-		with open(name,'wb') as f:
-			print("当前版本已过期，下载新版本中...")
-			response = requests.get(download_url % online_version, stream=True)
-			total_length = response.headers.get('content-length')
-
-			if total_length is None: # no content length header
-				f.write(response.content)
-			else:
-				dl = 0
-				total_length = int(total_length)
-				for data in response.iter_content(chunk_size=4096):
-					dl += len(data)
-					f.write(data)
-					done = int(50 * dl / total_length)
-					sys.stdout.write("\r[%s%s]" % ('#' * done, ' ' * (50-done)) )	
-					sys.stdout.flush()
-			print("下载完毕。")
-			input(f'请使用新版本 {name}\n点击Enter关闭视窗...')
-			exit()
+		print(f"当前版本[{local_version}]已过期，下载新版本[{online_version}]中...")
+		response = requests.get(download_url % online_version, stream=True)
+		total_size_in_bytes= int(response.headers.get('content-length', 0))
+		block_size = 1024 #1 Kibibyte
+		progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+		with open(name, 'wb') as file:
+			for data in response.iter_content(block_size):
+				progress_bar.update(len(data))
+				file.write(data)
+		progress_bar.close()
+		if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+			print("下载失败，请重新尝试。")
+		print("下载完毕。")
+		if os.path.getsize(name) != 0:
+			with zipfile.ZipFile(name,"r") as zip_ref:
+				zip_ref.extractall()
+			os.rename('default',f'WorkingTools-{online_version}')
+			os.startfile(f'WorkingTools-{online_version}')
+		input(f'请使用新版本 {name}\npress ENTER to quit...')
+		exit()
 
 
 def clean():
@@ -86,19 +93,44 @@ def login(driver):
 			code.clear()
 		except:
 			print("登入成功!")
+			go_system = WebDriverWait(driver, 20, 0.5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/ul/li/a')))
+			go_system.click()
 			break
-			
+
+def timeStamp(timeNum):
+	timeStamp = float(timeNum/1000)
+	timeArray = time.localtime(timeStamp)
+	timeStyle = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+	return timeStyle
+
+def get_responce_Info():
+	logs_raw = driver.get_log("performance")
+	logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
+	def log_filter(log_):
+			return (log_["method"] == "Network.responseReceived" and "json" in log_["params"]["response"]["mimeType"])
+	for log in filter(log_filter, logs):
+		request_id = log["params"]["requestId"]
+		resp_url = log["params"]["response"]["url"]
+		if resp_url == "http://fundmng.m6admin.com/api/manage/data/user/detail/by/username":
+			data = driver.execute_cdp_cmd("Network.getResponseBody", {"requestId": request_id})['body']
+			data = json.loads(data)
+			if data['message'].lower() == 'success':
+				try:
+					lastBetTime = timeStamp(data['data']['lastBettingTime'])
+					lastBet = f"{lastBetTime}"
+				except:
+					lastBet = "无投注"
+				#return usr_Info , lastBet
+				return lastBet
+			else:
+				return data['message']	
+
 def Game_inquiry(driver):
-	go_system = WebDriverWait(driver, 20, 0.5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/ul/li/a')))
-	go_system.click()
-	#report_page = WebDriverWait(driver, 20, 0.5).until(EC.visibility_of_element_located((By.XPATH, '//span[text()="报表查询"]')))
-	#report_page.click()
 	game_list = WebDriverWait(driver, 20, 0.5).until(EC.visibility_of_element_located((By.XPATH, '//a[text()="游戏注单查询"]')))
 	game_list.click()
 
-def sport_search(driver):
+def sport_search(driver,temp_list):
 	df = pd.read_excel("account.xlsx")
-	temp_list = []
 	before_username = ""
 	flow = ""
 	user_count = df.last_valid_index() + 1
@@ -135,20 +167,20 @@ def sport_search(driver):
 		temp_list.append(temp)
 		time.sleep(0.5)
 	end = pd.DataFrame(temp_list)
-	end.to_csv("流水.csv",encoding="utf_8_sig")
+	end.to_csv("数据.csv",encoding="utf_8_sig")
 	print("体育流水完成。")
+	return temp_list
 
-def Nsport_search(driver):
-	df = pd.read_csv("流水.csv")
+def Nsport_search(driver,temp_list,sportCheck):
+	df = pd.read_excel("account.xlsx")
 	a = df.last_valid_index()
-	temp_list = []
 	before_username = ""
 	flow = ""
 	user_count = df.last_valid_index() + 1
 	for i in tqdm(range(user_count)):
-		username = str(df.iat[i,1])
-		sport = float(df.iat[i,2]) if df.iat[i,2] != "" else ""
-		sport_win = float(df.iat[i,3]) if df.iat[i,3] != "" else ""
+		username = str(df.iat[i,0])
+		#sport = float(df.iat[i,2]) if df.iat[i,2] != "" else ""
+		#sport_win = float(df.iat[i,3]) if df.iat[i,3] != "" else ""
 		username_input = WebDriverWait(driver, 20, 0.5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="create-form_username"]')))
 		username_input.clear()
 		username_input.send_keys(username)
@@ -172,18 +204,72 @@ def Nsport_search(driver):
 			if before_username != username:
 				before_username == username
 				break
-		temp = {
-			'会员账号':username,
-			'体育流水': sport,
-			'体育输赢': sport_win,
-			'娱乐流水': float(flow) if flow else "",
-			'娱乐输赢': lose if lose else ""
-			}
-		temp_list.append(temp)
+		if temp_list != []:
+			if sportCheck:
+				userList = []
+				for UserDict in temp_list:
+					if UserDict['会员账号'] == username:
+						UserDict['娱乐流水'] = float(flow) if flow else ""
+						UserDict['娱乐输赢'] = lose if lose else ""
+			else:
+				temp = {
+					'会员账号':username,
+					'娱乐流水': float(flow) if flow else "",
+					'娱乐输赢': lose if lose else ""
+					}
+				temp_list.append(temp)
+		else:
+			temp = {
+				'会员账号':username,
+				'娱乐流水': float(flow) if flow else "",
+				'娱乐输赢': lose if lose else ""
+				}
+			temp_list.append(temp)
+
 		time.sleep(0.5)
 	end = pd.DataFrame(temp_list)
-	end.to_csv("流水.csv",encoding="utf_8_sig")
+	end.to_csv("数据.csv",encoding="utf_8_sig")
 	print("娱乐流水完成。")
+	return temp_list
+
+def user_info_search(driver,temp_list,sportCheck,NsportCheck):
+	df = pd.read_excel("account.xlsx")
+	a = df.last_valid_index()
+	before_username = ""
+	flow = ""
+	user_count = df.last_valid_index() + 1
+	for i in tqdm(range(user_count)):
+		username = str(df.iat[i,0])
+		input_username = WebDriverWait(driver, 20, 0.5).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="root"]/div/section/section/header[2]/div/div[2]/span/span/span[1]/input')))
+		input_username.clear()
+		input_username.send_keys(username)
+		input_username.send_keys(Keys.ENTER)
+		time.sleep(0.5)
+		lastBet = get_responce_Info()
+		if temp_list != []:
+			if sportCheck or NsportCheck:
+				userList = []
+				for UserDict in temp_list:
+					if UserDict['会员账号'] == username:
+						UserDict['最后投注时间'] = str(lastBet) if lastBet else ""
+			else:
+				temp = {
+					'会员账号': username,
+					'最后投注时间': str(lastBet) if lastBet else ""
+					}
+				temp_list.append(temp)
+		else:
+			temp = {
+			'会员账号': username,
+			'最后投注时间': str(lastBet) if lastBet else ""
+			}
+			temp_list.append(temp)
+		#print(temp_list)
+	print(len(temp_list))
+	end = pd.DataFrame(temp_list)
+	end.to_csv("数据.csv",encoding="utf_8_sig")
+	print("最后投注时间完成。")
+	return temp_list
 	
 def get_list(driver):
 	print("start")
@@ -233,7 +319,12 @@ def get_list(driver):
 	df.to_csv("result2.csv",encoding="utf_8_sig")
 	print("数据")
 	
-	
+
+def checkTrueFalse(check):
+	if check == 'y':
+		return True
+	elif check == 'n':
+		return False
 	
 if __name__ == "__main__":
 	check_version()
@@ -245,14 +336,43 @@ if __name__ == "__main__":
 		if version.lower() == 'ml':
 			domain = "http://fundmng.m6admin.com/login"
 			break
-	driver = webdriver.Chrome("chromedriver.exe",options=option)
+	driver = webdriver.Chrome(desired_capabilities=capabilities,executable_path="chromedriver.exe",options=option)
 	driver.get(domain)
 	login(driver)
-	Game_inquiry(driver)
-	input("請先點進 篩選時間&體育場館 -> 篩選完後小黑窗按Enter #不需要點擊查詢")
-	#get_list(driver)
-	sport_search(driver)
-	input("請篩選娱乐場館 -> 篩選完後小黑窗按Enter #不需要點擊查詢")
-	Nsport_search(driver)
-	input("流水已导出'流水.csv',可以关闭了")
+	while True:
+		while True:
+			sportCheck = input('是否需要查询体育流水(y/n)?：').lower()
+			if sportCheck in ['y','n']:
+				sportCheck = checkTrueFalse(sportCheck)
+				break
+		while True:
+			NsportCheck = input('是否需要查询娱乐流水(y/n)?：').lower()
+			if NsportCheck in ['y','n']:
+				NsportCheck = checkTrueFalse(NsportCheck)
+				break
+		while True:
+			lastBetTime = input('是否需要查询最后投注时间(y/n)?：').lower()
+			if lastBetTime in ['y','n']:
+				lastBetTime = checkTrueFalse(lastBetTime)
+				break
+		clean()
+		print(f'True => 是 \nFalse => 否\n\n体育流水查询：{sportCheck}\n娱乐流水查询：{NsportCheck}\n最后投注时间查询：{lastBetTime}')
+		doubleCheck = input('上述正确(y/n)?：').lower()
+		if doubleCheck == 'y':
+			break
+		else:
+			clean()
+	temp_list = []
+	if sportCheck:
+		Game_inquiry(driver)
+		input("請先篩選時間&體育場館 -> 篩選完後小黑窗按Enter #不需要點擊查詢")
+		temp_list = sport_search(driver,temp_list)
+	if NsportCheck:
+		if sportCheck != True:
+			Game_inquiry(driver)
+		input("請先篩選娱乐場館 -> 篩選完後小黑窗按Enter #不需要點擊查詢")
+		temp_list = Nsport_search(driver,temp_list,sportCheck)
+	if lastBetTime:
+		temp_list = user_info_search(driver,temp_list,sportCheck,NsportCheck)
+	input("数据已导出'数据.csv',可以关闭了")
 
